c73bcbf52c05ce5534ad7778f40849367a47e104
Danilo Alves da Silva Sales <danilo.alves@pixeon.com>
2022-03-07 19:58:53 -0300

    criação do avatar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,19 @@
+import * as S from './styles'
+
+
+type AvatarProps = {
+  imgUrl?: string;
+  nome: string;
+}
+
+const Avatar = ({imgUrl, nome}: AvatarProps) => {
+
+  return (
+    <S.AvatarWrapper>
+      <S.ImageWrapper src={imgUrl} alt={nome} />
+      <S.TextWrapper>{nome}</S.TextWrapper>
+    </S.AvatarWrapper>
+  )
+}
+
+export default Avatar;
---
+++ src/components/Avatar/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const AvatarWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`
+
+export const ImageWrapper = styled.img`
+  background: white;
+  border-radius: 100%;
+  width: 80px;
+  height: 80px;
+  max-width: 200px;
+  border: 6px solid black;
+`
+
+export const TextWrapper = styled.div`
+  color: black;
+  border: none;
+  font-weight: bold;
+  font-size: 36px;
+  text-align: center;
+  margin-left: 5px;
+
+`
